6f218a12fe23062741ab8b267c1d5aebcb5066d9
Dzmitry Liashko <dzliashko@gmail.com>
2026-06-19 21:50:27 +0300

    Add: day  finished

--- main.go
+++ main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Test")
+	name := "Go Blog"
+	url := "https://go.dev/blog/feed.atom"
+	articles := 15
+	active := true
+	rating := 4.9
+	subscribers := 42
+	averagePublicationsPerDay := 2.0
+
+	fmt.Printf("Feed: %s\n", name)
+	fmt.Printf("URL: %s\n", url)
+	fmt.Printf("Articles: %d\n", articles)
+	fmt.Printf("Active: %s\n", active)
+	fmt.Printf("Rating: %.2f\n", rating)
+	fmt.Printf("Rating: %.2f\n", rating)
+	fmt.Printf("Rating: %.2f\n", rating)
+	fmt.Printf("Subscribers: %d\n", subscribers)
+	fmt.Printf("Average publications per day: %.2f\n", averagePublicationsPerDay)
 }
