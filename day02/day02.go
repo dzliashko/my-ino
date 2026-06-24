e6ea955e872f358f2b9d76f09ebd712d37743066
Dzmitry Liashko <dzliashko@gmail.com>
2026-06-24 19:30:35 +0300

    Add: day 02 solved

---
+++ day02/day02.go
@@ -0,0 +1,25 @@
+package day02
+
+func IsPopular(subscribers int) bool {
+	if subscribers < 1000 {
+		return false
+	}
+	return true
+}
+
+func ChannelRating(subscribers int) string {
+	switch {
+	case subscribers < 100:
+		return "New"
+	case subscribers < 1_000:
+		return "Growing"
+	case subscribers < 10_000:
+		return "Popular"
+	default:
+		return "Top"
+	}
+}
+
+func AverageArticles(totalArticles int, days int) float64 {
+	return float64(totalArticles) / float64(days)
+}
