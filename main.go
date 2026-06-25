2f4942babb748e391d3377daf099506849926b9f
Dzmitry Liashko <dzliashko@gmail.com>
2026-06-25 17:17:17 +0300

    Add: day06 tasks solved

--- main.go
+++ main.go
@@ -1,6 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	goFeed := Feed{
+		Name:        "Go Blog",
+		URL:         "https://go.dev/blog/feed.atom",
+		Subscribers: 500,
+		Active:      true,
+		Articles:    10_000,
+	}
+	hnFeed := Feed{
+		Name:        "Hacker News",
+		URL:         "https://hnrss.org/frontpage",
+		Subscribers: 50_000,
+		Active:      true,
+		Articles:    100_000,
+	}
+
+	fmt.Print(goFeed.Info())
+	fmt.Print(hnFeed.Info())
+
+	fmt.Printf("%s -> %s\n", goFeed.Name, goFeed.Rating())
+	fmt.Printf("%s -> %s\n", hnFeed.Name, hnFeed.Rating())
+
+	fmt.Println(goFeed.IsPopular())
+	fmt.Println(hnFeed.IsPopular())
+
+	feeds := []Feed{goFeed, hnFeed}
+
+	for _, feed := range feeds {
+		feed.Info()
+	}
+
+	fmt.Println(totalSubscribers(feeds))
+
+	popular := popularFeeds(feeds)
+
+	for _, feed := range popular {
+		fmt.Println(feed.Name)
+	}
+
+	arcticle1 := Article{
+		Title:       "Go 1.25 Released",
+		Description: "string",
+		Link:        "string",
+		PublishedAt: time.Now(),
+		Feed:        goFeed,
+	}
+	arcticle2 := Article{
+		Title:       "Understanding Goroutines",
+		Description: "string",
+		Link:        "string",
+		PublishedAt: time.Now(),
+		Feed:        goFeed,
+	}
+	articles := []Article{arcticle1, arcticle2}
+
+	printArticles(articles)
+
+	articlesByFeed := (articlesByFeed(goFeed, articles))
+
+	for _, arcticle := range articlesByFeed {
+		fmt.Println(arcticle.ArticleInfo())
+	}
+}
 
 type Feed struct {
 	Name        string
@@ -61,42 +128,38 @@ func popularFeeds(feeds []Feed) []Feed {
 	return result
 }
 
-func main() {
-	goFeed := Feed{
-		Name:        "Go Blog",
-		URL:         "https://go.dev/blog/feed.atom",
-		Subscribers: 500,
-		Active:      true,
-		Articles:    10_000,
+type Article struct {
+	Title       string
+	Description string
+	Link        string
+	PublishedAt time.Time
+	Feed        Feed
+}
+
+func printArticles(articles []Article) {
+	for _, article := range articles {
+		fmt.Println("Feed:", article.Feed.Name)
+		fmt.Println("Title:", article.Title)
 	}
-	hnFeed := Feed{
-		Name:        "Hacker News",
-		URL:         "https://hnrss.org/frontpage",
-		Subscribers: 50_000,
-		Active:      true,
-		Articles:    100_000,
+}
+
+func articlesByFeed(feed Feed, articles []Article) []Article {
+	var result []Article
+	for _, article := range articles {
+		if article.Feed.Name == feed.Name {
+			result = append(result, article)
+		}
 	}
+	return result
+}
 
-	fmt.Print(goFeed.Info())
-	fmt.Print(hnFeed.Info())
-
-	fmt.Printf("%s -> %s\n", goFeed.Name, goFeed.Rating())
-	fmt.Printf("%s -> %s\n", hnFeed.Name, hnFeed.Rating())
-
-	fmt.Println(goFeed.IsPopular())
-	fmt.Println(hnFeed.IsPopular())
-
-	feeds := []Feed{goFeed, hnFeed}
-
-	for _, feed := range feeds {
-		feed.Info()
-	}
-
-	fmt.Println(totalSubscribers(feeds))
-
-	popular := popularFeeds(feeds)
-
-	for _, feed := range popular {
-		fmt.Println(feed.Name)
-	}
+func (f Article) ArticleInfo() string {
+	return fmt.Sprintf(
+		"Title: %s\nDescription: %s\nLink: %d\nPublishedAt: %v\nFeed: %v\n",
+		f.Title,
+		f.Description,
+		f.Link,
+		f.PublishedAt,
+		f.Feed,
+	)
 }
